3d5410b59daaa2b9633d5673e6f9b6e5d9b12828
WangGang <wanggang@zoncare.cn>
2016-09-29 11:02:13 +0800

    first commit

---
+++ app_d/src/main/java/com/example/app_d/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.app_d;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startA(View view) {
+        Intent intent = new Intent(this, com.example.library_a.InitActivity.class);
+        startActivity(intent);
+    }
+
+    public void startB(View view) {
+        Intent intent = new Intent(this, com.example.library_b.CompareActivity.class);
+        startActivity(intent);
+    }
+}
